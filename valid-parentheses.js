a590610df0c34a9d05c87358bbe66c63b7f2a0e9
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-06-12 21:05:46 +0300

    Find the valid parentheses
    Stack algorithm

---
+++ valid-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+
+    for(let idx = 0; idx < s.length; idx++){
+        if(s[idx] == '{'){
+            stack.push('}');
+        }
+        else if(s[idx] == '['){
+            stack.push(']');
+        }
+        else if(s[idx] == '('){
+            stack.push(')');
+        }
+    
+    
+    else if(stack.pop() !==s[idx]){
+        return false;
+    }
+
+    }
+    return !stack.length;
+};
+
+const s = "()";
+console.log(isValid(s).toString());
